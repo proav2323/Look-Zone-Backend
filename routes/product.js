be45e8ce5c4b042b7a75ddc4ccc0ba76d2b8f5f5
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-06-21 17:56:40 +0530

    products

--- routes/product.js
+++ routes/product.js
@@ -3,4 +3,18 @@ import { ObjectId } from "mongodb";
 
 const productRouter = express.Router();
 
+productRouter.get("/", async (req, res, next) => {
+  await req.db.createCollection("products");
+  let productsCollection = req.db.collection("products");
+
+  let productsCursor = productsCollection.find({});
+  let products = [];
+
+  for await (let product of productsCursor) {
+    products.push(product);
+  }
+
+  res.status(200).send(products);
+});
+
 export default productRouter;
